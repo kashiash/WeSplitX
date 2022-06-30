1676654cfd445e137db28da37c9b2a0fc70cdf46
kashiash <kashiash@gmail.com>
2022-06-30 23:12:21 +0200

    Initial Commit

---
+++ WeSplitX/WeSplitXApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitXApp.swift
+//  WeSplitX
+//
+//  Created by Jacek Placek on 30/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
